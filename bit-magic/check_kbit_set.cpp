6b6872fd13f9ac01b17c20b3923ea5093fdbb8be
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-09-08 22:12:32 +0000

    bit_magic codes

---
+++ bit-magic/check_kbit_set.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+bool right_shift_algo(int num, int k) {
+    return ((num >> (k-1)) & 1);
+}
+
+bool left_shift_algo(int num, int k) {
+    return ((1 << (k-1)) & num);
+}
+
+int main() {
+    int num, k;
+    std::cin >> num >> k;
+
+    std::cout << right_shift_algo(num, k) << std::endl;
+    std::cout << left_shift_algo(num, k) << std::endl;
+}
