24ca2f4295bc6a4a0f42ed64a612f11ff56314c7
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-09-09 01:27:16 +0000

    bit manipulation codes

--- bit-magic/two_odd_occuring.cpp
+++ bit-magic/two_odd_occuring.cpp
@@ -30,11 +30,35 @@ void twoodds(std::vector<int> nums) {
     }
 
     // Output
-    std::cout << g1 << " " << g2;
+    std::cout << g1 << " " << g2 << std::endl;
+}
+
+// XOR another approach (better)
+void twoodds2(std::vector<int> nums) {
+    // Do XOR of all elements
+    int x {0};
+    for (auto& v : nums) {
+        x = (x ^ v);
+    }
+    
+    // set bit mask generation
+    int u = x & ~(x-1);
+
+    // Group formation
+    int g1 {0}, g2{0};
+    for (auto& v : nums) {
+        if ((v & u)!=0) { g1 = g1 ^ v; }
+        else {g2 = g2 ^ v; }
+    }
+    
+    // Output
+    std::cout << g1 << " " << g2 << std::endl;
+
 }
 
 
 int main() {
     std::vector<int> nums = {9, 3, 3, 4, 4, 6, 6, 5, 5, 5, 5, 5};
     twoodds(nums);
+    twoodds2(nums);
 }
