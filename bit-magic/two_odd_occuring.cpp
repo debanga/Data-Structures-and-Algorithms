a4a7c56cb78b19a939f2895acde6838a3d4e039b
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-09-09 01:18:16 +0000

    bit manipulation codes

--- bit-magic/two_odd_occuring.cpp
+++ bit-magic/two_odd_occuring.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 
+// Check if k-th bit is set
 bool CheckSet(int m, int k) {
     return ((m >> (k-1)) & 1);
 }
@@ -14,21 +15,22 @@ void twoodds(std::vector<int> nums) {
     for (auto& v : nums) {
         x = (x ^ v);
     }
+
     // Find set key
     int k {1};
     while (!CheckSet(x, k)) {
         k++;
     }
+    
     // Group formation
     int g1 {0}, g2{0};
     for (auto& v : nums) {
         if (CheckSet(v, k)) { g1 = g1 ^ v; }
         else {g2 = g2 ^ v; }
     }
+
     // Output
     std::cout << g1 << " " << g2;
-
-
 }
 
 
