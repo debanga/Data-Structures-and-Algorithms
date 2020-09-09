7bfb9f0940b9034a45aa4b06cfa58b5ed7c67ef4
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-09-09 01:16:47 +0000

    bit manipulation codes

---
+++ bit-magic/two_odd_occuring.cpp
@@ -0,0 +1,38 @@
+// From a array of unsorted integer array, find the only two integers with
+// odd number of occurances
+#include <iostream>
+#include <vector>
+
+bool CheckSet(int m, int k) {
+    return ((m >> (k-1)) & 1);
+}
+
+// XOR Bucket: O(2n), S(1)
+void twoodds(std::vector<int> nums) {
+    // Do XOR of all elements
+    int x {0};
+    for (auto& v : nums) {
+        x = (x ^ v);
+    }
+    // Find set key
+    int k {1};
+    while (!CheckSet(x, k)) {
+        k++;
+    }
+    // Group formation
+    int g1 {0}, g2{0};
+    for (auto& v : nums) {
+        if (CheckSet(v, k)) { g1 = g1 ^ v; }
+        else {g2 = g2 ^ v; }
+    }
+    // Output
+    std::cout << g1 << " " << g2;
+
+
+}
+
+
+int main() {
+    std::vector<int> nums = {9, 3, 3, 4, 4, 6, 6, 5, 5, 5, 5, 5};
+    twoodds(nums);
+}
