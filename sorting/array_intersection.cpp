a6b0b08c6f81617b90d84d8a896f673eef04bfa8
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-09-12 18:08:42 +0000

    sorting algos

--- sorting/array_intersection.cpp
+++ sorting/array_intersection.cpp
@@ -4,7 +4,8 @@
 #include <unordered_map>
 using namespace std;
 
-void find_intersection(int arr1[], int arr2[], int m, int n) {
+// O(m+n), S(n)
+void find_intersection1(int arr1[], int arr2[], int m, int n) {
     int i {0}, j {0};
     unordered_map<int,int> umap;
     while(i<=m-1 && j<=n-1) {
@@ -34,9 +35,42 @@ void find_intersection(int arr1[], int arr2[], int m, int n) {
     cout << endl;
 }
 
+// O(m+n), S(1)
+void find_intersection2(int arr1[], int arr2[], int m, int n) {
+    int i {0}, j {0};
+    while(i<=m-1 && j<=n-1) {
+        if (arr1[i]<=arr2[j]) {
+            if (arr1[i] == arr2[j]) {
+                if (i>0 && arr1[i]==arr1[i-1]) {
+                    i++; 
+                    continue;
+                }
+                cout << arr1[i] << " ";
+            }
+            i++;
+        }
+        else {
+            j++;
+        }
+    }
+    /*
+    if (i==m) {
+        while (j<=n-1) {
+            j++;
+        }
+    }
+    if (j==n) {
+        while (i<=m-1) {
+            i++;
+        }
+    }
+    */
+    cout << endl;
+}
 
 int main() {
     int arr1[] = {1, 1, 2, 3, 3, 5};
     int arr2[] = {1, 1, 2, 2, 3, 4, 5};
-    find_intersection(arr1, arr2, 6, 7);
+    find_intersection1(arr1, arr2, 6, 7);
+    find_intersection2(arr1, arr2, 6, 7);
 }
