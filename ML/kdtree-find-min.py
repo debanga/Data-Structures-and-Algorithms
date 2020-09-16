73b3c0523b1e984e762d1d21b092d9766ee09671
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-09-16 22:53:42 +0000

    kdtree insert() and findmin()

--- ML/kdtree-find-min.py
+++ ML/kdtree-find-min.py
@@ -1,7 +1,12 @@
 # Find min in a kd tree in target dimension
 import numpy as np
 
-data = [[30, 40], [5,25], [10, 12], [70, 70], [50, 30], [35, 45]]
+data = [[30, 40], [5,25], [10, 12], [70, 70], [50, 30], [35, 45], [75, 80]]
+
+def draw_with_space(loc, txt):
+        for _ in range(loc):
+            print(" ", end=" ")
+        print(txt)
 
 class Node():
     def __init__(self, entry):
@@ -12,19 +17,21 @@ class Node():
         self.left = 0
         self.right = 0
 
-    def insert(self, entry, dim):
+    def insert(self, entry, dim, loc):
         if (entry[dim]<=self.root[dim]):
             if (self.left==0):
                 self.left = Node(entry)
-                print(str(entry) + " [L]")
+                draw_with_space(loc-3, str(entry))
             else:
-                self.left.insert(entry, (dim+1)%2)
+                loc = loc -3
+                self.left.insert(entry, (dim+1)%2, loc)
         else:
             if (self.right==0):
                 self.right = Node(entry)
-                print(str(entry) + " [R]")
+                draw_with_space(loc+3, str(entry))
             else:
-                self.right.insert(entry, (dim+1)%2)
+                loc = loc + 3
+                self.right.insert(entry, (dim+1)%2, loc)
 
     def find_minimum(self, dim, target_dim):
         if (dim==target_dim):
@@ -49,11 +56,13 @@ class Node():
 
 # Insert data to k-d tree x y x y x y ...
 tree = Node(data[0])
+draw_with_space(20, str(data[0]))
 for d in data[1:]:
-    tree.insert(d, 0)
+    tree.insert(d, 0, 20)
 print(" ")
 
-print(tree.find_minimum(0, 1))
+print("Min in dim 0" + str(tree.find_minimum(0, 0)))
+print("Min in dim 1" + str(tree.find_minimum(0, 1)))
 
 
 
