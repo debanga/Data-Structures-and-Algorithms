da84689eb4ec0d5585a083e36de266b636f90f6e
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-09-20 10:26:38 +0000

    work in progress

--- sorting/three_partition.cpp
+++ sorting/three_partition.cpp
@@ -13,59 +13,96 @@ void print_array(int arr[], int n) {
     cout << endl;
 }   
 
-void three_partition(int arr[], int n, int rl, int rh) {
-    int i {-1}, ii {-1};
-    int j {n}, jj {n};
-
+// Naive
+void three_partition(int arr[], int n, int rl, int rh) { //@buggy
+    int i {-1}, j {n};
     while (true) {
         do {
-            if (j==0) {break;}
-            j--;
-        }
-        while (arr[j]>rh);
-        jj = j;
-        do {
-            if (jj==0) {break;}
-            jj--;
-        }
-        while(arr[jj]<(rh+1));
-
-        if (jj==0) {break;}
-        // swap
-        cout << " " << arr[j] << " " << arr[jj] << " " << endl;
-        int t = arr[j];
-        arr[j] = arr[jj];
-        arr[jj] = t;   
-
-    }
-    print_array(arr, 8);
-    
-    while (true) {
-        do {
-            if (i==n-1) {break;}
             i++;
         }
-        while (arr[i]<rl || arr[i]>rh);
-        ii = i;
+        while (arr[i]<rl-0.5);
         do {
-            if (ii==n-1) {break;}
-            ii++;
+            j--;
         }
-        while(arr[ii]>rl);
-
-        if (ii==n-1) {break;}
+        while(arr[j]>rl-0.5);
+        if (i>=j) {break;}
         // swap
         int t = arr[i];
-        arr[i] = arr[ii];
-        arr[ii] = t;
+        arr[i] = arr[j];
+        arr[j] = t;   
     }
-    print_array(arr, 8);
+
+    i = -1;
+    j = n;
+    while (true) {
+        do {
+            i++;
+        }
+        while (arr[i]<rh+0.5);
+        do {
+            j--;
+        }
+        while(arr[j]>rh+0.5);
+        if (i>=j) {break;}
+        // swap
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;   
+    }   
+
+}
+
+// swap
+void swap(int& a, int &b) {
+    int t = a;
+    a = b;
+    b = t;
+}
+
+// Dutch National Flag
+void dutch_flag(int arr[], int n, int rl, int rh) {
+    // Define 3 pointer
+    int l = 0;
+    int m = 0;
+    int h = n-1;
+
+    010
+
+    // 0000 (l) 1111 0 (m) xxxx (h) 2222
+    while (m<=h) {
+        int c {0};
+
+        if (arr[m]<rl) { c=0; }
+        else if (arr[m]<rh+1) { c=1; }
+        else { c=2; }
+
+        switch (c) {
+            case 0: 
+                // swap (l,m)
+                swap(arr[l], arr[m]);
+                l++;
+                m++;
+                break;
+            case 1:
+                m++;
+                break;
+            case 2:
+                // swap (h,m)
+                swap(arr[h], arr[m]);
+                h--;
+                break;
+        }
+    }
 }
 
 
-int main() {
-    int arr[] = {2, 11, 2, 12, 10, 20, 10, 30};
 
-    three_partition(arr, 8, 10, 12);
+
+int main() {
+    int arr[] = {2, 10, 2, 12, 10, 20, 10, 30};
+
+    dutch_flag(arr, 8, 10, 11);
     print_array(arr, 8);
+
+
 }
