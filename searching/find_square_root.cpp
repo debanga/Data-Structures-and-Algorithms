f3de52fabad18a6196584b298f3b79d0d50fcf20
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-10-06 01:09:46 +0000

    find square root

--- searching/find_square_root.cpp
+++ searching/find_square_root.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 int find_sqroot(int x) {
-    if (x<0) { return -1;}
+    int ans = -1;
     int l = 0;
     int r = x;
     int mid;
@@ -17,18 +17,19 @@ int find_sqroot(int x) {
         }
 
         if (mid*mid < x) {
+            ans = mid;
             l = mid+1;
         }
         else if (mid*mid > x) {
             r = mid-1;
         }
     }
-    return min(l,r);
+    return ans;
 
 }
 
 
 int main() {
-    int x = -10;
+    int x = 21;
     cout << find_sqroot(x) << endl;
 }
