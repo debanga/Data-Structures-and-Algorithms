248f2bc392f1eee969d3f3b185733689d7e47dc0
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-10-27 17:16:50 -0400

    Update gfg-prob-count_only_repeated.cpp

--- searching/gfg-prob-count_only_repeated.cpp
+++ searching/gfg-prob-count_only_repeated.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 
+int binary_search(int arr[], int n, int k) {
+    int l = 0;
+    int r = n-1;
+    int mid;
+    
+    while(l<=r) {
+        mid = l + (r-l)/2;
+        
+        if (arr[mid] == k) {
+            return mid;
+        }
+        else if (arr[mid]<k) {
+            l = mid+1;
+        }
+        else {
+            r = mid-1;
+        }
+    }
+    
+    return -1;
+}
 
-int count_only_repeated(int arr[], int n) {
+
+pair<int, int> count_only_repeated(int arr[], int n) {
         // Find the repeating element 
     int l = 0;
     int r = n-1;
@@ -27,23 +49,24 @@ int count_only_repeated(int arr[], int n) {
         up = n;
     }
     else { // find up
-
+        up = binary_search(arr, n, res+1);
     }
 
     if (arr[0] == res) {
         low = -1;
     }
     else { // find low
-
+        low = binary_search(arr, n, res-1);
     }
 
-
-
+    pair<int, int> p {res, up - low - 1};
+    return p;
 }
 
 
 int main() {
-    int arr[] = {1, 2, 2, 3};
+    int arr[] = {1, 2, 3, 3};
     int n = 4;
-    cout << count_only_repeated(arr, n) << endl;
+    pair<int, int> p = count_only_repeated(arr, n);
+    cout << p.first << " " << p.second << endl;
 }
