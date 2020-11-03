ccd160131812fcb5ac73cebab44f595a14796d19
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-11-03 01:27:36 +0000

    strings: leftmost_repeating

--- strings/leftmost_repeating.cpp
+++ strings/leftmost_repeating.cpp
@@ -21,7 +21,36 @@ int leftmost_repeat_index(string str) {
 }
 
 
+int leftmost_repeat_index_1(string str) {
+    int count[CHAR] = {0};
+    int map[CHAR];
+    fill(map, map+CHAR, -1);
+
+    for (int i=0; i<str.length(); ++i) {
+        if (map[str[i]-'a']==-1) {
+            map[str[i]-'a']=i;
+        }
+
+        count[str[i]-'a']++;
+    }
+
+    int min = 100000;
+    int idx = -1;
+    for (int i=0; i<str.length(); ++i) {
+        if (count[i]>1 && map[i]<min) {
+            min = map[i];
+            idx = i;
+        }
+    }
+    if (idx>-1) {
+        return idx;
+    }
+    return -1;
+}
+
+
 int main() {
-    string str = "abcdedd";
+    string str = "abbdedd";
     cout << leftmost_repeat_index(str) << endl;
+    cout << leftmost_repeat_index_1(str) << endl;
 }
