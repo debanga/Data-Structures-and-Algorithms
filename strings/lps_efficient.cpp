3e2bf38fa005ce7f40787e943dec5ca511ec0b95
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-11-10 03:05:15 +0000

    efficient lps computation

---
+++ strings/lps_efficient.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void FindLPS(string str) {
+    int lps[str.length()];
+    int len = 0;
+    lps[0] = 0;
+
+    int i = 1;
+    while (i<str.length()) {
+        if (str[i]==str[len]) {
+            lps[i] = ++len;
+            i++;
+        }
+        else {
+            if (len==0) {
+                lps[i] = 0;
+                i++;
+            }
+            else {
+                len = lps[len-1];
+            }
+        }
+    }
+
+    for (int i=0; i<str.length(); ++i) {
+        cout << lps[i] << " ";
+    }
+    cout << endl;
+
+}
+
+
+int main() {
+    string str = "abcbabca";
+    FindLPS(str);
+
+}
